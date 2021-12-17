30bad1ba7513ba277226a828a4f0ced19a0965b2
Bennett <bjupfield@gmail.com>
2021-12-17 11:07:23 -0600

    commit tree c694ae3107ff105bc9f9d5ea7115c31be84e8739

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
-
+require_relative './user'
+class Student < User
+    @knowledge = Array.[];
+    def learn
+        if(@knowledge === nil)
+            @knowledge = [KNOWLEDGE.sample]
+        else
+            @knowledge.push(KNOWLEDGE.sample)
+        end
+    end
+    def knowledge
+        @knowledge
+    end
 end
